2ac1d22c1834071fe290fd88189547991c07d20f
Aswin Krishna <your_email@example.com>
2026-03-26 17:54:20 +0530

    fixed content

--- app/data/data.js
+++ app/data/data.js
@@ -6,7 +6,7 @@ export const projects = [
         image: "/image/samsara.webp",
         overview:
             "Samsara AI is designed to automate workflows using AI-driven logic. It focuses on improving productivity by simplifying repetitive tasks and providing intelligent suggestions.",
-        tech: ["Next.js", "Tailwind CSS", "AI"],
+        tech: ["Next.js", "Tailwind CSS", "Gemini Ai" , "JavaScript" , ],
         features: [
             "AI-based workflow automation",
             "Clean and responsive UI",
@@ -14,8 +14,8 @@ export const projects = [
         ],
         challenges:
             "Handling dynamic data flow and integrating AI responses efficiently while maintaining performance and UI consistency.",
-        github: "https://github.com/your-link",
-        live: "https://your-live-link",
+        github: "https://github.com/aaswne/samsara-ai-chatbot",
+        live: "https://samsara-ai-chatbot.vercel.app",
     },
 
     {
@@ -25,7 +25,7 @@ export const projects = [
         image: "/image/Netflix.webp",
         overview:
             "A full-stack Netflix clone that replicates the core UI and functionality of Netflix.",
-        tech: ["React", "Node.js", "MongoDB"],
+        tech: ["React", "Tmtb", "JavaScript", "Axios", "Vite"],
         features: [
             "User authentication",
             "Dynamic movie listing",
@@ -33,18 +33,18 @@ export const projects = [
         ],
         challenges:
             "Managing authentication securely and handling state across components.",
-        github: "https://github.com/your-link",
-        live: "https://your-live-link",
+        github: "https://github.com/aaswne/Netflix-Clone",
+        live: "https://aaswne.github.io/Netflix-Clone/",
     },
 
     {
         id: "olx-clone",
-        title: "OLX Clone",
+        title: "Full Stack Marketplace",
         description: "Marketplace app for buying and selling products.",
         image: "/image/olx.webp",
         overview:
-            "A marketplace platform where users can list and browse products.",
-        tech: ["Next.js", "Firebase"],
+            "A full-stack marketplace platform modeled after OLX, allowing users to list, browse, and purchase products. Features include real-time updates, authentication, and smooth user experience.",
+        tech: ["Next.js", "Firebase", "Firestore", "JavaScript"] ,
         features: [
             "Product listing system",
             "User authentication",
@@ -52,7 +52,7 @@ export const projects = [
         ],
         challenges:
             "Handling real-time data updates and ensuring smooth UX.",
-        github: "https://github.com/your-link",
-        live: "https://your-live-link",
+        github: "https://github.com/aaswne/olx-clone-react-firebase",
+        live: "",
     },
 ];
